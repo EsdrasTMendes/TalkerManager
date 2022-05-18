a0430e0f22bd670487f94289d1c7e1a8c8045a77
Esdras <esdrastmendes@gmail.com>
2022-05-18 17:25:26 -0300

    feat: validação da rota /login. Email válido e senha com no mínimo 6 caracteres

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ const readFiles = async () => {
   const file = await fs.readFile('talker.json', 'utf8');
   return JSON.parse(file);
 };
+const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const functionRegex = RegExp(regex);
+// Referencias https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+
+const validateLogin = (request, response, next) => {
+  const { email, password } = request.body;
+  if (!password) {
+    response.status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    response.status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  if (!email) {
+    response.status(400)
+      .json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!functionRegex.test(email)) {
+    response.status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
 
 const token = () => crypto.randomBytes(8).toString('hex');
 
@@ -42,7 +66,7 @@ app.get('/talker/:id', async (request, response) => {
     .json(filteredTalkers);
 });
 
-app.post('/login', (request, response) => {
+app.post('/login', validateLogin, (request, response) => {
   const { email, password } = request.body;
   users.push(email, password);
   return response.status(HTTP_OK_STATUS).json({ token: token() });
