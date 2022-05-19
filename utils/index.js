5c4dae8bc7953825c682951e49f76cfbf6b6c478
Esdras <esdrastmendes@gmail.com>
2022-05-18 22:37:41 -0300

    feat: criação da função para escrita em arquivos writeFiles

--- utils/index.js
+++ utils/index.js
@@ -7,7 +7,14 @@ const readFiles = async () => {
   const file = await fs.readFile('talker.json', 'utf8');
   return JSON.parse(file);
 };
+
+const writeFiles = async (write) => {
+  const data = await fs.writeFile('talker.json', JSON.stringify(write));
+  return data;
+};
+
 module.exports = {
   token,
   readFiles,
+  writeFiles,
 };
